643d019e4f236a0cdcb550a72fcaa3c801510130
Santiago <santiagoperez.-@hotmail.com>
2016-09-02 22:20:52 -0300

    Actualizado 1
    lalal

---
+++ source/Reg.hx
@@ -0,0 +1,18 @@
+package;
+
+/**
+ * ...
+ * @author ...
+ */
+class Reg
+{
+	static public var highscore:Int = 0;
+	
+	inline static public var playerVelX:Float = 300;
+	
+	public function new() 
+	{
+		
+	}
+	
+}
